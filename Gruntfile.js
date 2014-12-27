bb399e72f2269d71b834127672303d58c29684a0
Pierre Rudloff <contact@rudloff.pro>
2014-12-27 14:34:56 +0100

    Add a grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,22 @@ module.exports = function (grunt) {
                     'dist/main.css': ['css/style.css']
                 }
             }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['uglify']
+            },
+            styles: {
+                files: ['css/*.css'],
+                tasks: ['cssmin']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['uglify', 'cssmin']);
 };
