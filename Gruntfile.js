47809e407a6c135241127f75a27cfceaacf33115
Pierre Rudloff <contact@rudloff.pro>
2015-01-18 04:08:15 +0100

    PHPCS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,37 @@
 /*jslint node: true */
 module.exports = function (grunt) {
     'use strict';
-    grunt.initConfig({
-        uglify: {
-            combine: {
-                files: {
-                    'dist/main.js': ['js/mnccolors.js', 'js/map.js']
-                },
-                options: {
-                    sourceMap: true
+    grunt.initConfig(
+        {
+            uglify: {
+                combine: {
+                    files: {
+                        'dist/main.js': ['js/mnccolors.js', 'js/map.js']
+                    },
+                    options: {
+                        sourceMap: true
+                    }
                 }
-            }
-        },
-        cssmin: {
-            combine: {
-                files: {
-                    'dist/main.css': ['css/style.css']
-                }
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['js/*.js'],
-                tasks: ['uglify']
             },
-            styles: {
-                files: ['css/*.css'],
-                tasks: ['cssmin']
+            cssmin: {
+                combine: {
+                    files: {
+                        'dist/main.css': ['css/style.css']
+                    }
+                }
+            },
+            watch: {
+                scripts: {
+                    files: ['js/*.js'],
+                    tasks: ['uglify']
+                },
+                styles: {
+                    files: ['css/*.css'],
+                    tasks: ['cssmin']
+                }
             }
         }
-    });
+    );
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
