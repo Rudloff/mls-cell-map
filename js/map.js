20523c5d74a52dfebeff5a98f4cf554bb4110e1c
Pierre Rudloff <contact@rudloff.pro>
2015-01-18 04:34:55 +0100

    PHPCS

--- js/map.js
+++ js/map.js
@@ -19,23 +19,27 @@ function showPopup(feature, layer) {
     } else {
         color = 'white';
     }
-    layer.bindPopup('<b>CID</b>: ' + feature.properties.cell +
-        '<br/><b>MNC</b>: ' + feature.properties.net +
-        '<br/><b>MCC</b>: ' + feature.properties.mcc +
-        '<br/><b>LAC</b>: ' + feature.properties.area +
-        '<br/><b>Operator</b>: ' + feature.properties.operator +
-        '<br/><b>Country</b>: ' + feature.properties.country +
-        '<br/><b>Type</b>: ' + feature.properties.radio +
-        '</br></br><b>Latitude</b>: ' + feature.geometry.coordinates[1] +
-        '</br><b>Longitude</b>: ' + feature.geometry.coordinates[0] +
-        '</br><b>Range</b>: ' + feature.properties.range + ' m' +
-        '<br/><br/><i>' + feature.properties.samples + '</i> measurements' +
-        '</br><b>Created</b>: ' + new Date(feature.properties.created * 1000).toISOString() +
-        '</br><b>Updated</b>: ' + new Date(feature.properties.updated * 1000).toISOString(),
-        { autoPan: false });
-    layer.options.icon = L.AwesomeMarkers.icon({
-        markerColor: color
-    });
+    layer.bindPopup(
+        '<b>CID</b>: ' + feature.properties.cell +
+            '<br/><b>MNC</b>: ' + feature.properties.net +
+            '<br/><b>MCC</b>: ' + feature.properties.mcc +
+            '<br/><b>LAC</b>: ' + feature.properties.area +
+            '<br/><b>Operator</b>: ' + feature.properties.operator +
+            '<br/><b>Country</b>: ' + feature.properties.country +
+            '<br/><b>Type</b>: ' + feature.properties.radio +
+            '</br></br><b>Latitude</b>: ' + feature.geometry.coordinates[1] +
+            '</br><b>Longitude</b>: ' + feature.geometry.coordinates[0] +
+            '</br><b>Range</b>: ' + feature.properties.range + ' m' +
+            '<br/><br/><i>' + feature.properties.samples + '</i> measurements' +
+            '</br><b>Created</b>: ' + new Date(feature.properties.created * 1000).toISOString() +
+            '</br><b>Updated</b>: ' + new Date(feature.properties.updated * 1000).toISOString(),
+        { autoPan: false }
+    );
+    layer.options.icon = L.AwesomeMarkers.icon(
+        {
+            markerColor: color
+        }
+    );
     layer.on('click', displayCircle);
 }
 
@@ -52,10 +56,15 @@ function showMarkers(e) {
     'use strict';
     if (e.target.readyState === 4 && e.target.status === 200) {
         var cells = JSON.parse(e.target.response);
-        markers.addLayer(L.geoJson(cells, {
-            onEachFeature: showPopup,
-            filter: addNewMarkers
-        }));
+        markers.addLayer(
+            L.geoJson(
+                cells,
+                {
+                    onEachFeature: showPopup,
+                    filter: addNewMarkers
+                }
+            )
+        );
     }
 }
 
@@ -93,70 +102,83 @@ function goToCell(e) {
     }
 }
 
-var SearchCellControl = L.Control.extend({
-    initialize: function (options) {
-        'use strict';
-        L.Util.setOptions(this, options);
-    },
-    searchCell: function () {
-        'use strict';
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = goToCell;
-        ajax.open('GET', 'ajax/searchCell.php?mcc=' + document.getElementById('mcc').value + '&mnc=' + document.getElementById('mnc').value + '&lac=' + document.getElementById('lac').value + '&cell_id=' + document.getElementById('cell_id').value);
-        ajax.send(null);
-    },
-    onAdd: function () {
-        'use strict';
-        var container = L.DomUtil.create('div', 'search-cell-control'), fields = ['MCC', 'MNC', 'LAC', 'Cell ID'], i, id, field, label, input, br, submitBtn = L.DomUtil.create('button', '');
-        for (i = 0; i < fields.length; i += 1) {
-            id = fields[i].toLowerCase().replace(' ', '_');
-            field = L.DomUtil.create('div', 'cellsearch-line');
-            label = L.DomUtil.create('label', 'cellsearch-label');
-            label.textContent = fields[i];
-            label.setAttribute('for', id);
-            field.appendChild(label);
-            input = L.DomUtil.create('input', 'cellsearch-input');
-            input.setAttribute('type', 'number');
-            input.setAttribute('id', id);
-            field.appendChild(input);
-            br = L.DomUtil.create('br', '');
-            field.appendChild(br);
-            container.appendChild(field);
+var SearchCellControl = L.Control.extend(
+    {
+        initialize: function (options) {
+            'use strict';
+            L.Util.setOptions(this, options);
+        },
+        searchCell: function () {
+            'use strict';
+            var ajax = new XMLHttpRequest();
+            ajax.onreadystatechange = goToCell;
+            ajax.open('GET', 'ajax/searchCell.php?mcc=' + document.getElementById('mcc').value + '&mnc=' + document.getElementById('mnc').value + '&lac=' + document.getElementById('lac').value + '&cell_id=' + document.getElementById('cell_id').value);
+            ajax.send(null);
+        },
+        onAdd: function () {
+            'use strict';
+            var container = L.DomUtil.create('div', 'search-cell-control'), fields = ['MCC', 'MNC', 'LAC', 'Cell ID'], i, id, field, label, input, br, submitBtn = L.DomUtil.create('button', '');
+            for (i = 0; i < fields.length; i += 1) {
+                id = fields[i].toLowerCase().replace(' ', '_');
+                field = L.DomUtil.create('div', 'cellsearch-line');
+                label = L.DomUtil.create('label', 'cellsearch-label');
+                label.textContent = fields[i];
+                label.setAttribute('for', id);
+                field.appendChild(label);
+                input = L.DomUtil.create('input', 'cellsearch-input');
+                input.setAttribute('type', 'number');
+                input.setAttribute('id', id);
+                field.appendChild(input);
+                br = L.DomUtil.create('br', '');
+                field.appendChild(br);
+                container.appendChild(field);
+            }
+            submitBtn.textContent = 'Search';
+            submitBtn.addEventListener('click', this.searchCell, false);
+            container.appendChild(submitBtn);
+            L.DomEvent.disableClickPropagation(container);
+            return container;
         }
-        submitBtn.textContent = 'Search';
-        submitBtn.addEventListener('click', this.searchCell, false);
-        container.appendChild(submitBtn);
-        L.DomEvent.disableClickPropagation(container);
-        return container;
     }
-});
+);
 
 function init() {
     'use strict';
     map = L.map('map',  { minZoom: 11, maxZoom: 18 }).setView([48.57457, 7.75875], 13);
     map.zoomControl.setPosition('topright');
-    L.control.locate({
-        position: 'topright'
-    }).addTo(map);
+    L.control.locate(
+        {
+            position: 'topright'
+        }
+    ).addTo(map);
 
-    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }),
-        coverage = L.tileLayer('https://d17pt8qph6ncyq.cloudfront.net/tiles/{z}/{x}/{y}.png', {
+    var osm = L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+            attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }
+    ), coverage = L.tileLayer(
+        'https://d17pt8qph6ncyq.cloudfront.net/tiles/{z}/{x}/{y}.png',
+        {
             maxNativeZoom: 13,
             attribution: '<a target="_blank"  href="https://location.services.mozilla.com/">Mozilla Location Service</a>'
-        });
+        }
+    );
     markers = new L.MarkerClusterGroup({ disableClusteringAtZoom: 18 }).addTo(map);
     osm.addTo(map);
     coverage.addTo(map);
     L.control.layers({ 'OSM': osm }, { 'Coverage': coverage, 'Cells': markers }).addTo(map);
     map.on('moveend', getMarkers);
     getMarkers();
-    map.addControl(new L.Control.Geocoder({
-        collapsed: false,
-        geocoder: new L.Control.Geocoder.Nominatim({ serviceUrl: 'https://nominatim.openstreetmap.org/' }),
-        position: 'topleft'
-    }));
+    map.addControl(
+        new L.Control.Geocoder(
+            {
+                collapsed: false,
+                geocoder: new L.Control.Geocoder.Nominatim({ serviceUrl: 'https://nominatim.openstreetmap.org/' }),
+                position: 'topleft'
+            }
+        )
+    );
     map.addControl(L.control.scale());
     map.addControl(new L.Control.Permalink({ useLocation: true, text: null }));
     map.addControl(new SearchCellControl({position: 'topleft'}));
